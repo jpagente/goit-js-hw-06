c5de94047e7f1d8aeaade552a2270a4d48469130
Judielyn Pagente <git-commit-address>
2024-01-24 23:59:47 +0800

    Task 4 and 5 commit

---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const nameInput = document.getElementById("name-input");
+    const nameOutput = document.getElementById("name-output");
+
+    nameInput.addEventListener("input", () => {
+    const inputValue = nameInput.value.trim();
+
+    if (inputValue === "") {
+        nameOutput.textContent = "Anonymous";
+    } else {
+        nameOutput.textContent = inputValue;
+    }
+    });
+});
+
+
